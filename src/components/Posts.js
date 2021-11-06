78de625bfed63bf86bf2d65200661b5104e4308f
lamara <lamaralanchava@gmail.com>
2021-11-06 17:43:59 +0300

    added structure

---
+++ src/components/Posts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "./Post";
+
+export default ({posts}) => {
+  if(!posts.length) {
+    return (
+      <p className='text-center'>No Posts</p>
+    )
+  }
+  return (
+
+    posts.map(post => <Post post={post} key={post}/>)
+  )
+}
