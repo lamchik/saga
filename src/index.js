393e150bee2729ec31edae75f2d8f07b55d24c8a
lamara <lamaralanchava@gmail.com>
2021-11-08 23:21:23 +0300

    async posts done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import thunk from 'redux-thunk'
 import reportWebVitals from './reportWebVitals';
-import {compose, createStore} from "redux";
+import App from './App';
+import {compose, createStore, applyMiddleware} from "redux";
 import {rootReducer} from "./redux/rootReducer";
 import {Provider} from "react-redux";
 
 const store = createStore(rootReducer, compose (
+  applyMiddleware(
+    thunk
+  ),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
